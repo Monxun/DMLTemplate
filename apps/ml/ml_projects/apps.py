adc5cb5f9fb7ba75c012e9007cd33c0dc3656acf
Monxun <michaelgibney1@gmail.com>
2021-12-25 21:12:10 -0700

    added ml / data sub app dir + ml_projects, datasets, and streams apps

---
+++ apps/ml/ml_projects/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class MlProjectsConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'apps.ml.ml_projects'
+    label = 'apps_ml_ml_projects'
