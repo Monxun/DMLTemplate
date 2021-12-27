badf6d1263b626cda602c1173e255c4450ed20d8
Michael Gibney <87790334+Monxun@users.noreply.github.com>
2021-12-26 21:39:41 -0700

    Delete apps directory

--- apps/ml/ml_projects/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class MlProjectsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'apps.ml.ml_projects'
-    label = 'apps_ml_ml_projects'
